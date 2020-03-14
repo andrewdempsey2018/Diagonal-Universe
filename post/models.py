4391c40b10898b613b8ea0fb7a9f0e2def329204
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-03-14 15:54:29 +0000

    Added blog post model

--- post/models.py
+++ post/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=300)
+    subtitle = models.TextField(max_length=300)
     category = models.CharField(max_length=20)
     date = models.DateField()
-    text = models.CharField(max_length=3000)
+    text = models.TextField(max_length=3000)
     # comments
-    topPicphoto = models.ImageField(upload_to="cars")
-    middlePicphoto = models.ImageField(upload_to="cars")
+    topPicphoto = models.ImageField(upload_to="images")
+    middlePicphoto = models.ImageField(upload_to="images")
     middlePicDescription = models.CharField(max_length=300)
