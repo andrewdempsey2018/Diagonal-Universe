5ca502dd14a3084f79f80184cc5067bb49d23999
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-03-17 18:32:42 +0000

    Added index page view

--- post/views.py
+++ post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Import the Story model for use
+from .models import Post
+
+def index(request):
+    return render(request, "index.html")
