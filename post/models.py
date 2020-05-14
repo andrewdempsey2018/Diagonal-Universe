c32e896db9694a9b2aa5fd2acd9205c3e9e3e396
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-05-14 13:50:43 +0100

    Updated layout of index page

--- post/models.py
+++ post/models.py
@@ -5,13 +5,27 @@ class Post(models.Model):
     subtitle = models.TextField(max_length=300)
     category = models.CharField(max_length=20)
     date = models.DateField()
-    text = models.TextField(max_length=3000)
-    # comments
-    topPicphoto = models.CharField(max_length=100)
-    topPicphotoDescription = models.CharField(max_length=100, default="Description of picture")
+    
+    topImage = models.CharField(max_length=100)
+    topImageDescription = models.CharField(max_length=100, default="Description of picture")
 
-    middlePicphoto = models.CharField(max_length=100)
-    middlePicphotoDescription = models.CharField(max_length=300, default="Description of picture")
+    text1 = models.TextField(max_length=3000, default="No text")
 
-    youtube = models.CharField(max_length=100, default="No video for this post")
-    github = models.CharField(max_length=100, default="No repo for this post")
+    middleImage = models.CharField(max_length=100)
+    middleImageDescription = models.CharField(max_length=300, default="Description of picture")
+
+    text2 = models.TextField(max_length=3000, default="No text")
+
+    extraImage1 = models.CharField(max_length=100, default="No image")
+    extraImage1Description = models.CharField(max_length=100, default="Description of picture")
+
+    extraImage2 = models.CharField(max_length=100, default="No image")
+    extraImage2Description = models.CharField(max_length=100, default="Description of picture")
+
+    extraImage3 = models.CharField(max_length=100, default="No image")
+    extraImage3Description = models.CharField(max_length=100, default="Description of picture")
+
+    video = models.CharField(max_length=100, default="No video for this post")
+    repo = models.CharField(max_length=100, default="No repo for this post")
+
+    # User comments to be coded
