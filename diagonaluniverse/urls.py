73aaf5bde9b560582f7be69e832b120faca7f4a7
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-05-21 11:07:34 +0100

    Activated navbar links

--- diagonaluniverse/urls.py
+++ diagonaluniverse/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", include("post.urls")),
     path("projects", include("projects.urls")),
     path("about", views.about, name="about"),
+    path("contact", include("ContactForm.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
