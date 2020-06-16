f6d46e3e11b4a50d5d4cf3060a85b72ddaa5d5cb
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-06-16 19:55:58 +0100

    Added project detail view

--- projects/views.py
+++ projects/views.py
@@ -6,3 +6,7 @@ from .models import Project
 def project_list(request):
     projects = Project.objects.all()
     return render(request, "projects/project_list.html", {'projects': projects})
+
+def project_detail(request, slug):
+    project = Project.objects.get(slug=slug)
+    return render(request, "projects/project_detail.html", {'project': project})
