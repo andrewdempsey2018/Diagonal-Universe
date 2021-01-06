fb63261af70bf57deb23423e46e4aa5229a69551
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-01-06 19:13:58 +0000

    Updated post model to allow more flexibility with posts

--- post/models.py
+++ post/models.py
@@ -25,28 +25,10 @@ class Post(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORYS, default=TECH)
     date = models.DateField(default=datetime.now)
     
-    topImage = models.CharField(max_length=100, default="No image")
-    topImageDescription = models.CharField(max_length=100, default="Description of picture")
+    text1 = models.TextField(max_length=7000, default="No text")
 
-    text1 = models.TextField(max_length=3000, default="No text")
-
-    middleImage = models.CharField(max_length=100, default="No image")
-    middleImageDescription = models.CharField(max_length=300, default="Description of picture")
-
-    text2 = models.TextField(max_length=3000, default="No text")
-
-    extraImage1 = models.CharField(max_length=100, default="No image")
-    extraImage1Description = models.CharField(max_length=100, default="Description of picture")
-
-    extraImage2 = models.CharField(max_length=100, default="No image")
-    extraImage2Description = models.CharField(max_length=100, default="Description of picture")
-
-    extraImage3 = models.CharField(max_length=100, default="No image")
-    extraImage3Description = models.CharField(max_length=100, default="Description of picture")
+    text2 = models.TextField(max_length=7000, default="No text")
 
     video = models.CharField(max_length=100, default="No video for this post")
-    repo = models.CharField(max_length=100, default="No repo for this post")
-
-    references = models.TextField(max_length=1000, default="No references")
 
     # User comments to be coded
