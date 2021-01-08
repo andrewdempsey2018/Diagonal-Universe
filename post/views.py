2aa0813d7532a63f03f14997b2037b8ff194d2b3
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-01-08 11:24:45 +0000

    Refactor index page and views to use more descriptive variable names

--- post/views.py
+++ post/views.py
@@ -7,7 +7,7 @@ from Comment.models import Comment
 from Comment.forms import CommentForm
 
 def index(request):
-    latestPost = Post.objects.latest('id')
+    post = Post.objects.latest('id')
     categorys = Post.CATEGORYS
 
     #remove 'projects' from categorys as we dont want this listed on main page
@@ -18,7 +18,7 @@ def index(request):
     #print(new)
     
     #get comments unique to the latest post
-    comments = Comment.objects.all().filter(subject=latestPost.slug)
+    comments = Comment.objects.all().filter(subject=post.slug)
 
     form = CommentForm()
 
@@ -27,11 +27,40 @@ def index(request):
         comment = Comment()
         comment.name = request.POST.get('name')
         comment.text = request.POST.get('text')
-        comment.subject = latestPost.slug
+        comment.subject = post.slug
         comment.save()
         return render(request, 'post/thanks.html')  
     else:
-        return render(request, "post/index.html", {'latestPost': latestPost, 'categorys': categorys, 'comments': comments, 'form': form})
+        return render(request, "post/index.html", {'post': post, 'categorys': categorys, 'comments': comments, 'form': form})
+
+def old_post(request, slug):
+    #messy code here (entire view should be refactored)
+    #old_post view and index view should be the same function
+    post = Post.objects.get(slug=slug)
+    categorys = Post.CATEGORYS
+
+    #remove 'projects' from categorys as we dont want this listed on main page
+    new = list(categorys)
+    new.remove(('Projects', 'Projects'))
+    print("!")
+    categorys = tuple(new)
+    print(new)
+    
+    #get comments unique to the latest post
+    comments = Comment.objects.all().filter(subject=post.slug)
+
+    form = CommentForm()
+
+    if request.method == 'POST':
+        #if form.is_valid():
+        comment = Comment()
+        comment.name = request.POST.get('name')
+        comment.text = request.POST.get('text')
+        comment.subject = post.slug
+        comment.save()
+        return render(request, 'post/thanks.html')  
+    else:
+        return render(request, "post/index.html", {'post': post, 'categorys': categorys, 'comments': comments, 'form': form})
 
 def thanks(request):
     return render(request, "post/thanks.html")
@@ -43,39 +72,6 @@ def post_list(request, cat):
 def project_list(request):
     postList = Post.objects.filter(category = 'Projects')
     return render(request, "post/post_list.html", {'postList': postList})
-
-#def old_post(request, slug):
-#    postToView = Post.objects.get(slug=slug)
-#    return render(request, "post/old_post.html", {'postToView': postToView})
-
-def old_post(request, slug):
-    #messy code here (entire view should be refactored)
-    #old_post view and index view should be the same function
-    latestPost = Post.objects.get(slug=slug)
-    categorys = Post.CATEGORYS
-
-    #remove 'projects' from categorys as we dont want this listed on main page
-    new = list(categorys)
-    new.remove(('Projects', 'Projects'))
-    print("!")
-    categorys = tuple(new)
-    print(new)
-    
-    #get comments unique to the latest post
-    comments = Comment.objects.all().filter(subject=latestPost.slug)
-
-    form = CommentForm()
-
-    if request.method == 'POST':
-        #if form.is_valid():
-        comment = Comment()
-        comment.name = request.POST.get('name')
-        comment.text = request.POST.get('text')
-        comment.subject = latestPost.slug
-        comment.save()
-        return render(request, 'post/thanks.html')  
-    else:
-        return render(request, "post/index.html", {'latestPost': latestPost, 'categorys': categorys, 'comments': comments, 'form': form})
 
 def new_post(request):
     if request.method == 'POST':
