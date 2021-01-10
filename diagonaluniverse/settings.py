252e38bdbb0b148478862e60cb3c79f39b1333ac
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-01-10 14:35:02 +0000

    Updateed whitenoise settings

--- diagonaluniverse/settings.py
+++ diagonaluniverse/settings.py
@@ -102,7 +102,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Additional static files
 STATICFILES_DIRS = [
