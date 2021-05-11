cd01fc25ce7aa669a53bcf70d0d8ba13b391a35d
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-05-11 20:34:30 +0100

    Updated post categorys

--- post/models.py
+++ post/models.py
@@ -8,7 +8,6 @@ class Post(models.Model):
     PROJECTS = 'Projects'
     COMPUTING = 'Computing'
     CULTURE = 'Culture'
-    THOUGHTS = 'Thoughts'
 
     CATEGORYS = (
         (TECH, 'Tech'),
@@ -16,7 +15,6 @@ class Post(models.Model):
         (PROJECTS, 'Projects'),
         (COMPUTING, 'Computing'),
         (CULTURE, 'Culture'),
-        (THOUGHTS, 'Thoughts'),
     )
 
     title = models.CharField(max_length=100)
