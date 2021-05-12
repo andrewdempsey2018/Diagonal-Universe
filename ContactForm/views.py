0f726869fb94955ae44283376def93dc51121840
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-05-12 19:29:16 +0100

    Fixed redirect after user sends a contact message

--- ContactForm/views.py
+++ ContactForm/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 
+from django.urls import reverse
+
+from post import views
+
 from .models import ContactModel
 
 from .forms import ContactModelForm
@@ -12,7 +16,7 @@ def contact(request):
         form = ContactModelForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(reverse(views.view_post))
     else:
         form = ContactModelForm()
     return render(request, "ContactForm/contact.html", {'form': form})
